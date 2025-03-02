a44bd2e2e318edf72537b3d839594a0629eacc84
Lai The Thang <laithethang00@gmail.com>
2025-03-02 20:54:32 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -9,4 +9,14 @@ public class HelloController {
     public String index() {
         return "Hello World";
     }
+
+    @GetMapping("/user")
+    public String getUser() {
+        return "Hello World e";
+    }
+
+    @GetMapping("/haha")
+    public String getHaha() {
+        return "Hello World b";
+    }
 }
